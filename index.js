95edca2bc4d7e4c7750c42640aae2bf7bdeb0dd5
srsklrboii <35755454+srsklrboii@users.noreply.github.com>
2018-03-28 07:12:48 +1100

    Update index.js

--- index.js
+++ index.js
@@ -383,109 +383,6 @@ var straightrate = [
     "100",
 ]
 
-var incestrate= [
-    "1",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "11",
-    "12",
-    "13",
-    "14",
-    "15",
-    "16",
-    "17",
-    "18",
-    "19",
-    "20",
-    "21",
-    "22",
-    "23",
-    "24",
-    "25",
-    "26",
-    "27",
-    "28",
-    "29",
-    "30",
-    "31",
-    "32",
-    "33",
-    "34",
-    "35",
-    "36",
-    "37",
-    "38",
-    "39",
-    "40",
-    "41",
-    "42",
-    "43",
-    "44",
-    "45",
-    "46",
-    "47",
-    "48",
-    "49",
-    "50",
-    "51",
-    "52",
-    "53",
-    "54",
-    "55",
-    "56",
-    "57",
-    "58",
-    "59",
-    "60",
-    "61",
-    "62",
-    "63",
-    "64",
-    "65",
-    "66",
-    "67",
-    "68",
-    "69",
-    "70",
-    "71",
-    "72",
-    "73",
-    "74",
-    "75",
-    "76",
-    "77",
-    "78",
-    "79",
-    "80",
-    "81",
-    "82",
-    "83",
-    "84",
-    "85",
-    "86",
-    "87",
-    "88",
-    "89",
-    "90",
-    "91",
-    "92",
-    "93",
-    "94",
-    "95",
-    "96",
-    "97",
-    "98",
-    "99",
-    "100",
-]
-
 var waifurater = [
     "1",
     "2",
@@ -1081,30 +978,7 @@ bot.on("message", async function(message) {
             console.error(e)
         })
         break;
-
-        case "incestrate":
-        var incestmember = message.mentions.users.first()
-        if (!incestmember) {
-            var embed = new Discord.RichEmbed()
-                .setAuthor("Incest Rate")
-                .addField("Incest rate below :regional_indicator_w: :regional_indicator_o: :regional_indicator_w:", `You are ${incestrate[Math.floor(Math.random() * incestrate.length)]}% incest.`)
-                .setFooter("Created by srsklrboii#5784")
-                .setColor("RANDOM")
-            message.channel.send(embed).catch(e => {
-                console.error(e)
-            })
-            return
-        }
-        var incestembed = new Discord.RichEmbed()
-            .setAuthor("Incest Rate")
-            .addField("Incest rate below :regional_indicator_w: :regional_indicator_o: :regional_indicator_w:", `${incestmember.username} is ${incestrate[Math.floor(Math.random() * incestrate.length)]}% incest.`)
-            .setFooter("Created by srsklrboii#5784")
-            .setColor("RANDOM")
-        message.channel.send(incestembed).catch(e => {
-            console.error(e)
-        })
-        break;
-
+        
         case "waifurate":
         var waifumember = message.mentions.users.first()
         if (!waifumember) {
